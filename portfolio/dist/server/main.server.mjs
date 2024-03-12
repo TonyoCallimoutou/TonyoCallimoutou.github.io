be80509751bd072a208fb6ec02afbd722b66d3b6
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2024-03-13 00:48:24 +0100

    init prod

--- portfolio/dist/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-SBPETW2Z.mjs";import"./chunk-ISV22Q7K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- portfolio/src/app/page/about/event.ts
+++
@@ -1,7 +0,0 @@
-export interface EventItem {
-  category: "degree" | "experience";
-  title: string;
-  date: string;
-  where: string;
-  description: string;
-}
